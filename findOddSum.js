a19edfd788144b9f871ba0a6e85f32aa1638e1ab
gautamsen117 <gautamsen117@gmail.com>
2022-09-15 19:28:21 +0600

    odd number sum

--- findOddSum.js
+++ findOddSum.js
@@ -4,9 +4,27 @@ function getSumOfAnArray(numbers){
         const index = i;
         const element  = numbers[index];
         sum = sum + element;
-        console.log(index, element, sum);
+        // console.log(index, element, sum);
     }
     return sum;
 }
+
+function getOddNumberOfAnArray(numbers){
+    const oddNumbers = [];
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element % 2 !== 0){
+            console.log(index, element);
+            oddNumbers.push(element);
+        }
+    }
+    return oddNumbers;
+}
+
+
 const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-getSumOfAnArray(myNumbers);
+const oddNumbers = getOddNumberOfAnArray(myNumbers)
+console.log(oddNumbers);
+const oddNumberSum = getSumOfAnArray(oddNumbers);
+console.log(oddNumberSum);
